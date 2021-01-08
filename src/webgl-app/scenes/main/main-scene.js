fb51cfb6a10fa1039cf944764746ae0b01ea2916
Amelie <amelierosser1986@gmail.com>
2021-01-08 21:59:33 +0000

    cleanup

--- src/webgl-app/scenes/main/main-scene.js
+++ src/webgl-app/scenes/main/main-scene.js
@@ -1,9 +1,7 @@
 import BaseScene from '../base/base-scene';
-import { VECTOR_ZERO } from '../../utils/math';
 import assets from './assets';
 import Raytracer from './objects/raytracer/raytracer';
 import { postProcessing } from '../../rendering/renderer';
-import { Vector3 } from 'three';
 
 export const MAIN_SCENE_ID = 'main';
 
@@ -11,8 +9,8 @@ export default class MainScene extends BaseScene {
   constructor() {
     super({ id: MAIN_SCENE_ID, assets, gui: true, guiOpen: true, controls: true });
     this.cameras.main.position.set(3, 3, 2);
-    this.cameras.main.lookAt(new Vector3(0, 0, -1));
     this.control.target.set(0, 0, -1);
+    this.control.update();
   }
 
   /**
